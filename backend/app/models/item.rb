094a15934cbc8afe12363269b05cf7fccb4ee92f
Christophe Dubois <113714569+chridubois@users.noreply.github.com>
2023-04-06 15:41:45 +0000

    random image on item when item created without image

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require "ruby/openai"
 
 class Item < ApplicationRecord
   belongs_to :user
@@ -17,14 +16,5 @@ class Item < ApplicationRecord
 
   before_validation do
     self.slug ||= "#{title.to_s.parameterize}-#{rand(36**6).to_s(36)}"
-    unless self.image
-      openai = OpenAI::Client.new(access_token: "#{ENV["OPENAI_API_KEY"]}")
-      response = openai.Image.create(
-        prompt="#{title.to_s}",
-        n=1,
-        size="256x256"
-      )
-      self.image = response['data'][0]['url']
-    end
   end
 end
